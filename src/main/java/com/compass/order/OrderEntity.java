12747c5495404f49ea47fffd0b21ebde73192b4d
Davidson <davidson.05@outlook.com>
2024-07-04 09:57:20 -0300

    add validacao na ItemEntity

--- src/main/java/com/compass/order/OrderEntity.java
+++ src/main/java/com/compass/order/OrderEntity.java
@@ -4,6 +4,8 @@ import com.compass.item.ItemEntity;
 import com.compass.order.enums.StatusOrder;
 import com.compass.shelter.ShelterEntity;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -21,18 +23,23 @@ public class OrderEntity implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Temporal(TemporalType.DATE)
     @Column(nullable = false)
+    @NotNull(message = "Data do pedido é obrigatória")
     private Date date;
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
+    @NotNull(message = "Status do pedido é obrigatório")
     private StatusOrder status;
 
     @ManyToOne
     @JoinColumn(name = "shelter_id")
+    @NotNull(message = "Abrigo do pedido é obrigatório")
     private ShelterEntity shelter;
 
     @OneToMany(cascade = CascadeType.ALL)
     @JoinColumn(name = "ordem_id")
+    @NotEmpty(message = "A lista de itens do pedido não pode ser vazia ou nula")
     private List<ItemEntity> items;
 }
